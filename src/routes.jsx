453b55dcb868eeca00036b05afc380aa2f446edf
fermb02 <155971382+fermb02@users.noreply.github.com>
2025-07-02 14:16:14 -0400

    Añadí la ruta /catalogo como pública y mejore la navegacion de las rutas para la pagina
    Envuelve todo en <BrowserRouter>, de lo contrario las rutas no funcionan. Tambien, importa BrowserRouter junto a Routes y Route.

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,38 +1,54 @@
-// src/Routes.jsx
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import LandingPage      from "./pages/LandingPage";
 import LoginPage        from "./pages/LoginPage";
 import ModificarUsuario from "./pages/ModificarUsuario";
 import ReservasPage     from "./pages/ReservasPage";
+import CatalogoPage     from "./pages/CatalogoPage";
+import ReservarEspacio  from "./pages/ReservarEspacio";
+
 import ProtectedRoute   from "./components/ProtectedRoute";
-import ReservarEspacio from "./pages/ReservarEspacio";
 
 export default function AppRoutes() {
   return (
-    <Routes>
-      {/* Públicas */}
-      <Route path="/"       element={<LandingPage />} />
-      <Route path="/login"  element={<LoginPage />} />
+    <BrowserRouter>
+      <Routes>
+        {/* Rutas Públicas */}
+        <Route path="/"      element={<LandingPage />} />
+        <Route path="/login" element={<LoginPage />} />
 
-      {/* Protegidas: sólo accesibles si hay auth.currentUser */}
-      <Route
-        path="/modificarusuario"
-        element={
-          <ProtectedRoute>
-            <ModificarUsuario />
-          </ProtectedRoute>
-        }
-      />
-      <Route
-        path="/mis-reservas"
-        element={
-          <ProtectedRoute>
-            <ReservasPage />
-          </ProtectedRoute>
-        }
-      />
-    </Routes>
+        {/* Catálogo no requiere login */}
+        <Route path="/catalogo" element={<CatalogoPage />} />
+
+        {/* Reservar puede requerir usuario autenticado */}
+        <Route
+          path="/reservar"
+          element={
+            <ProtectedRoute>
+              <ReservarEspacio />
+            </ProtectedRoute>
+          }
+        />
+
+        {/* Rutas Protegidas */}
+        <Route
+          path="/modificarusuario"
+          element={
+            <ProtectedRoute>
+              <ModificarUsuario />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/mis-reservas"
+          element={
+            <ProtectedRoute>
+              <ReservasPage />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
-// Nota: Si necesitas más rutas públicas o protegidas, simplemente añádelas aquí siguiendo el mismo patrón.
