c6af0f2a3e2b43a693b3a9d59aa80e630b00dbb4
Cesar Atencio <167229343+cesargatencio@users.noreply.github.com>
2025-07-07 16:31:53 -0400

    detalle

--- src/components/EspacioCard/EspacioCard.jsx
+++ src/components/EspacioCard/EspacioCard.jsx
@@ -18,7 +18,7 @@ const EspacioCard = ({ espacio }) => (
     <h3>{espacio.nombre}</h3>
     <p>{espacio.descripcion}</p>
     <p><strong>Capacidad:</strong> {espacio.capacidad}</p>
-    <p><strong>Precio:</strong> {espacio.precio}</p>
+    <p><strong>Precio:</strong> {espacio.precio} $/H</p>
     <Link to={`/espacios/${espacio.id}`} className="btn-ver-detalles">
       <button className="btn-ver-detalles">Ver Detalles</button>
     </Link>
